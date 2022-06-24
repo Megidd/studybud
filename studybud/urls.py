4a33577b358ae5192ecbb3d9f87861d8f0c3eddd
Megidd Git <megiddgit@gmail.com>
2022-06-24 13:23:33 +0430

    Another sample url & view

--- studybud/urls.py
+++ studybud/urls.py
@@ -20,7 +20,11 @@ from django.http import HttpResponse
 def home(request):
     return HttpResponse('Home page')
 
+def room(request):
+    return HttpResponse('ROOM')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home)
+    path('', home),
+    path('room/', room),
 ]
