ace4e4512652a89ca501875afc02d7575410a91c
Megidd Git <megiddgit@gmail.com>
2022-07-20 06:12:33 +0430

    Log user in if username & password are correct:
    It creates `sessionid` for DB and for browser storage.

--- base/views.py
+++ base/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.db.models import Q
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
 from .models import Room, Topic
 from .forms import RoomForm
@@ -17,6 +18,14 @@ def loginPage(request): # Don't use `login` for function name due to conflict.
             user = User.objects.get(username=username)
         except:
             messages.error(request, 'User does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or password does not exist')
 
     context = {}
     return render(request, 'base/login_register.html', context)
