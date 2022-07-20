e7e3f0bcc7abb03ff178242e061dc282df2b72d2
Megidd Git <megiddgit@gmail.com>
2022-07-20 07:10:08 +0430

    Prevent user from re-login

--- base/views.py
+++ base/views.py
@@ -13,6 +13,9 @@ from .forms import RoomForm
 # Create your views here.
 
 def loginPage(request): # Don't use `login` for function name due to conflict.
+    if request.user.is_authenticated:
+        return redirect('home')
+
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
