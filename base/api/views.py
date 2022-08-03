84dff69f57a61397c272053ecf4fdc28d6154c14
Megidd Git <megiddgit@gmail.com>
2022-08-03 17:29:04 +0430

    Test: change import way: it works

--- base/api/views.py
+++ base/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from base.models import Room
-from .serializers import RoomSerializer
+from base.api.serializers import RoomSerializer
 
 @api_view(['GET'])
 def getRoutes(request):
