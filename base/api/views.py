c13f12c024726a988526688b992e9244aaac57fa
Megidd Git <megiddgit@gmail.com>
2022-08-03 15:46:02 +0430

    URL & view for API: get rooms

--- base/api/views.py
+++ base/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .. import models
 
 @api_view(['GET'])
 def getRoutes(request):
@@ -9,3 +10,11 @@ def getRoutes(request):
         'GET /api/rooms/:id'
     ]
     return Response(routes)
+
+@api_view(['GET'])
+def getRooms(request):
+    rooms = models.Room.objects.all()
+    rooms_names = []
+    for room in rooms:
+        rooms_names.append(room.name)
+    return Response(rooms_names)
