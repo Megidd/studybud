96487801d0e9347db2e44dffceee5ecda8842fce
Megidd Git <megiddgit@gmail.com>
2022-08-04 14:24:33 +0430

    Remove all previous models, sqlite DB, migrations:
    Relative path:
    
    db.sqlite3
    base/migrations/0001_initial.py
    base/migrations/0002_topic_room_host_message_room_topic.py
    base/migrations/0003_alter_room_options_room_participants.py
    
    Absolute path:
    
    /home/m3/repos/django-7-hours/studybud/db.sqlite3
    /home/m3/repos/django-7-hours/studybud/base/migrations/0001_initial.py
    /home/m3/repos/django-7-hours/studybud/base/migrations/0002_topic_room_host_message_room_topic.py
    /home/m3/repos/django-7-hours/studybud/base/migrations/0003_alter_room_options_room_participants.py

--- base/models.py
+++ base/models.py
@@ -1,38 +1,3 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 # Create your models here.
-
-class Topic(models.Model):
-    name = models.CharField(max_length=200)
-
-    def __str__(self) -> str:
-        return self.name
-
-class Room(models.Model):
-    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
-    name = models.CharField(max_length=200)
-    description = models.TextField(null=True, blank=True) # null for database. blank for form.
-    participants = models.ManyToManyField(User, related_name='participants', blank=True) # Blank for submitting form. `related_name` to avoid conflict with another `User` relationship.
-    updated = models.DateTimeField(auto_now=True)
-    created = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        ordering = ['-updated', '-created']
-
-    def __str__(self) -> str:
-        return self.name
-
-class Message(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    room = models.ForeignKey(Room, on_delete=models.CASCADE) # models.SET_NULL would keep messages.
-    body = models.TextField() # A body is required.
-    updated = models.DateTimeField(auto_now=True)
-    created = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        ordering = ['-updated', '-created']
-
-    def __str__(self) -> str:
-        return self.body[0:50]
