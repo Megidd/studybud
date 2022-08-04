5982b7fd5973ca2edf6a4ec042a001a0fcdf491a
Megidd Git <megiddgit@gmail.com>
2022-08-04 17:55:13 +0430

    Modify user creation form: consider new user model

--- base/views.py
+++ base/views.py
@@ -3,11 +3,10 @@ from django.http import HttpResponse
 from django.contrib import messages
 from django.db.models import Q
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from .models import Room, Topic, Message, User
-from .forms import RoomForm, UserForm
+from .forms import RoomForm, UserForm, MyUserCreationForm
 
 # Create your views here.
 
@@ -43,10 +42,10 @@ def logoutUser(request):
 def registerPage(request):
     page = 'register'
 
-    form = UserCreationForm()
+    form = MyUserCreationForm()
 
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = MyUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
             user.username = user.username.lower() # Make sure it's lower case.
